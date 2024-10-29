f09c8fc85c27d53a96383d755e2e48092ff4441d
kietdev <kiet.nguyenanh1@ninjavan.co>
2024-10-29 14:22:00 +0700

    Fix code conventions

--- google_wrapper/utils/utils.py
+++ google_wrapper/utils/utils.py
@@ -6,4 +6,3 @@ def get_service_account(private_key_id: str) -> ServiceAccount:
         return ServiceAccount.objects.get(private_key_id=private_key_id)
     except ServiceAccount.DoesNotExist:
         raise Exception(f'Service account {private_key_id} not found in the database.')
-
--- opv2/base/service.py
+++ opv2/base/service.py
@@ -11,11 +11,12 @@ from django.utils import timezone
 from core.patterns import SingletonMeta
 from google_wrapper.models import ServiceAccount
 from google_wrapper.services import GoogleSheetService, GoogleChatService
+from google_wrapper.utils import get_service_account
 from google_wrapper.utils.card_builder import CardBuilder
 from google_wrapper.utils.card_builder import widgets as W
 from google_wrapper.utils.card_builder.elements import CardHeader, Section
 from stos.utils import Configs
-from google_wrapper.utils import get_service_account
+
 
 class TokenManager(metaclass=SingletonMeta):
     """
