93db8b2477052b7a0262e68f2c3454439944b010
kietdev <kiet.nguyenanh1@ninjavan.co>
2024-10-30 10:51:29 +0700

    add loki log

--- google_wrapper/apps.py
+++ google_wrapper/apps.py
@@ -11,9 +11,7 @@ logger = logging.getLogger(__name__)
 
 
 def start_pubsub_listener():
-    while True:
-        logger.info("Pub/Sub listener is running...")
-        time.sleep(2)
+    pass
 
 
 class GoogleWrapperConfig(AppConfig):
@@ -22,7 +20,7 @@ class GoogleWrapperConfig(AppConfig):
 
     def ready(self):
         # Check if the process is gunicorn or runserver
-        if os.environ.get('RUN_MAIN') == 'true' or 'gunicorn' in sys.argv or '--noreload' in sys.argv:
+        if os.environ.get('RUN_MAIN') == 'true' or 'gunicorn' in sys.argv[0] or '--noreload' in sys.argv:
             logger.info("Starting Pub/Sub listener in web server process...")
             # Thread(target=start_pubsub_listener, daemon=True).start()
         else:
